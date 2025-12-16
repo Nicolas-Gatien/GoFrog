b786783dbf21d3b674d0cd289a47c548013515fb
Nicolas Gatien <nicolas.gatien6@gmail.com>
2025-12-15 20:24:35 -0500

    🐁 Add Mouse Position

--- main.go
+++ main.go
@@ -23,6 +23,9 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	background := color.RGBA{103, 114, 169, 255}
 	screen.Fill(background)
 
+	cursorX, cursorY := ebiten.CursorPosition()
+	ebitenutil.DebugPrintAt(screen, "Here", cursorX, cursorY)
+
 	opts := ebiten.DrawImageOptions{}
 	opts.GeoM.Translate(float64((GameWidth/2.0)-(g.FROG_IMAGE.Bounds().Dx()/2.0)), float64((GameHeight/2.0)-(g.FROG_IMAGE.Bounds().Dy()/2.0)))
 	screen.DrawImage(g.FROG_IMAGE, &opts)
